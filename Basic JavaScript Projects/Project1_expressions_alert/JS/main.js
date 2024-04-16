d1defc00b82f9462334530c750204f792e7309c7
rickysanchez1 <richard.sanchez1983@gmail.com>
2024-04-16 14:55:33 -0700

    Added alerts, expressions, functions and concatenated strings

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,2 +1,24 @@
-let A = "STRING";
-window.alert(A);
+let A = "Hello \"this is how to wrote quotes\" \"This is how you write \\ \"";
+let B = "First part. " + "hello! this is the second part."
+let bevvy = "Soda", Pepsi = "Cola", DrPepper = "another cola", Coke = "Same as Pepsi", Sprite = "Lemon/Lime Soda", LaCroix = "Soda Water";
+let express = 50 + 50;
+let Sent1 = "This is the beginning of the string"; //Second assignment
+let Sent2 = " and this is the end of the string"; // Second assignment
+
+
+window.alert(A + B); // First Assignment
+
+document.write(Sprite) // First Assignment
+
+document.write(Sent1 + Sent2) // Third assignment
+
+document.write(express) // Fourth Assignment
+
+function myFirstfunction() { //Defining a function and naming it
+    let str = "Gotcha!"; //Defining a variable and giving it a string value
+    document.getElementById("BUTTONTEXT").innerHTML = str; // “document.getElementById” is a method that returns an element.
+                                                            //The element has an ID attribute with a specific value assigned to it. 
+                                                            //It is used mainly to control or get information from an element within
+                                                            //your code. If it can’t find the element with the specified value, it 
+                                                            //will return “null.”
+}
