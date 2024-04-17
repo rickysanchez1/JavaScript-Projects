05f87f35a2ef9fdd3d286c4969c291ae4340bdbe
rickysanchez1 <richard.sanchez1983@gmail.com>
2024-04-17 09:23:57 -0700

    Removed redundancies

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,5 +1,3 @@
-let car = "Volvo"
-
 function firstFunction() { //Declared a variable, firstFunction
     let x = 6; // Assigned 6 value to x variable
     let y = 7; // Assigned 7 value to y variable
@@ -12,12 +10,10 @@ function myFunction() {
     document.getElementById('letsadd').innerHTML = sentence;
 }
 
-let collect = document.getElementsByTagName("p");
-
 function functionOne(x, y) {
     return x * y;
 }
 
 function secondFunction() {
     document.getElementById("two").style.color = "red";
-  }
+}
