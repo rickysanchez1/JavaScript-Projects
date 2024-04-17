7365f6015bc6c586024af330658c8f29c2b9bf04
rickysanchez1 <richard.sanchez1983@gmail.com>
2024-04-17 11:07:34 -0700

    Added functions with multiple math expressions

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,5 +1,24 @@
 function additionFunc() {
     let addition = 8 + 7;
-    document.getElementById("Math").innerHTML = "8 + 7 = " + addition;
+    document.getElementById("addmath").innerHTML = "8 + 7 = " + addition;
 }
 
+function subtractionFunc() {
+    let Subtraction = 5 - 2;
+    document.getElementById("submath").innerHTML = "5 - 2 = " + Subtraction;
+}
+
+function multiplyFunc() {
+    let multiply = 15 * 3;
+    document.getElementById("multmath").innerHTML = "15 * 3 = " + multiply;
+}
+
+function divideFunc() {
+    let divide = 8 / 4;
+    document.getElementById("divmath").innerHTML = "8 / 4 = " + divide;
+}
+
+function moreMath() {
+    let simplemath = (23 + 15) * 43 / 4 - 9;
+    document.getElementById("math").innerHTML = "23 plus 15, multiplied by 43, divided by 5 and then subtracted by 9 equals " + simplemath;
+}
