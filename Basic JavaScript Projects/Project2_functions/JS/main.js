d213001646e288e877ebe6a328f5f23ea19e8a55
rickysanchez1 <richard.sanchez1983@gmail.com>
2024-04-17 00:03:57 -0700

    Added mew function with concatenated string

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,8 +1,11 @@
-
-
-
 function firstFunction() {
     let x = 6;
     let y = 7;
     document.getElementById("button").innerHTML = x + y;
+}
+
+function myFunction() {
+    let sentence = "I am the first part of the sentence";
+    sentence += " I was concatenated";
+    document.getElementById('letsadd').innerHTML = sentence;
 }
