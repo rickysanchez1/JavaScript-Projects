2512e62c4f852db3ebfa9729052de1f3408fea74
rickysanchez1 <richard.sanchez1983@gmail.com>
2024-04-17 17:09:23 -0700

    Added dictionary project

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,25 @@
+function myDictionary() {
+    let Brandon = { // Creating Key
+        career:"Glazier", // These are all values
+        age:"43", // These are all values
+        height:"5'11", // These are all values
+        hair:"short", // These are all values
+        children:"3" // These are all values
+    };
+    let Sydney = { // Created another key for the dictionary with its values below
+        career:"Doctor",
+        age:"35",
+        height:"5'10",
+        hair:"long",
+        children:"2"
+    };
+    let Karla = {
+        career:"Student",
+        age:"15",
+        height:"5'6",
+        hair:"medium",
+        children:"0"
+    };
+    delete Brandon.career;
+    document.getElementById("Dictionary").innerHTML = Brandon.career;
+}
