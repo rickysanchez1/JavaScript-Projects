2e270385264e35747bec2ba85bacabf5c54a86a6
rickysanchez1 <richard.sanchez1983@gmail.com>
2024-04-19 20:33:29 -0700

    Added 2 functions using this and new operator

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,7 +1,7 @@
-// Tenary Operators syntax: variable = (condition) ? expressiontrue : expressionfalse;
+// Ternary Operators syntax: variable = (condition) ? expressiontrue : expressionfalse;
 function rideFunction() {
     let height, can_ride;
-    height = document.getElementById("height").value;// Tenary operators operate on 3 operands.
+    height = document.getElementById("height").value;// Ternary operators operate on 3 operands.
     can_ride = (height < 52) ? "You are not tall enough":"You are tall enough"; // Tenary operators can be used to replace multiple lines of code with a single line.
     document.getElementById("ride").innerHTML = can_ride + " to ride.";
 }
@@ -9,7 +9,36 @@ document.write(Bigger = (6 > 1) ? "Left is bigger than the right number":"Right 
 
 function rockthevote() {
     let age, canvote;
-    age = document.getElementById("age").value;// Tenary operators operate on 3 operands.
-    canvote = (age < 18) ? "You are not old enough ":"You are old enough "; // Tenary operators can be used to replace multiple lines of code with a single line.
-    document.getElementById("vote").innerHTML = canvote + " to vote.";
+    age = document.getElementById("age").value;
+    canvote = (age < 18) ? "You are not old enough ":"You are old enough ";
+    document.getElementById("vote").innerHTML = canvote + " to rock the vote.";
+}
+
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make; // "this" keyword refers to the current object in this method.
+    this.Vehicle_Model = Model; // "this" keyword offers a template until the new object is created from the constructor and use the values of the created object
+    this.Vehicle_Year = Year; // 
+    this.Vehicle_Color = Color;
+}
+
+let Bonnie = new Vehicle("Ford", 730, 1934, "White"); // "new" Keyword is used to create objects from the function that is using them.
+let Orenthal = new Vehicle("Ford", "Bronco", 1993, "White"); // each variable here has defined values for each method in the function that will be using it.
+let Steve = new Vehicle("Ford", "Mustang", 1968, "Dark Green");
+function carFunction() {
+    document.getElementById("carinfo").innerHTML = "Bonnie and her friend Clyde drive a " + Bonnie.Vehicle_Color + "-colored " + Bonnie.Vehicle_Make + " " + Bonnie.Vehicle_Model + " manufactured in " + Bonnie.Vehicle_Year;
+}
+
+function Manga(Author, Series, Run, Genre) {
+    this.Manga_Author = Author; // "this" keyword refers to the current object in this method.
+    this.Manga_Series = Series; // "this" keyword offers a template until the new object is created from the constructor and use the values of the created object
+    this.Manga_Run = Run;
+    this.Manga_Genre = Genre;
+}
+
+let Alyssa = new Manga("Hirohiko Araki", "Jojo's Bizarre Adventure", 1987, "Adventure"); // The variable name will take place in "this" in the function it is referred to.
+let Jordan = new Manga("Katsuhiro Otomo", "Akira", 1982, "Cyberpunk");
+let Ricky = new Manga("Naoki Urasawa", "Pluto", 2003, "Science Fiction Thriller");
+let Amber = new Manga("Kazuo Umezu", "Drifint Classroom", 1972, "Horror");
+function mangaFunction() {
+    document.getElementById("New_and_this").innerHTML = "Ricky's favorite Manga is " + Ricky.Manga_Series + " by author " + Ricky.Manga_Author + ". It was first published in the year " + Ricky.Manga_Run + " and the genre is " + Ricky.Manga_Genre;
 }
